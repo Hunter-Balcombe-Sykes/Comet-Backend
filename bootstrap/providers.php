173ddf9077854d18324fbbd098cc01a4174e07c5
jhunter7333 <jhunter7333@gmail.com>
2026-06-27 16:25:48 +1000

    style(integrations): import provider class in bootstrap/providers.php (pint)

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,11 +4,12 @@ use App\Providers\AppServiceProvider;
 use App\Providers\BotProtectionServiceProvider;
 use App\Providers\DatabaseServiceProvider;
 use App\Providers\EventServiceProvider;
+use App\Providers\PlatformRegistryServiceProvider;
 
 return [
     AppServiceProvider::class,
     DatabaseServiceProvider::class,
     EventServiceProvider::class,
     BotProtectionServiceProvider::class,
-    App\Providers\PlatformRegistryServiceProvider::class,
+    PlatformRegistryServiceProvider::class,
 ];
